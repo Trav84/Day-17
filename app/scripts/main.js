a895a087177dc5e16a4d417d6a69def4939f4afa
Travis Czerw <travis.czerw@gmail.com>
2015-01-29 16:21:30 -0600

    Front end talking to back end server

---
+++ app/scripts/main.js
@@ -0,0 +1,61 @@
+/* jshint devel:true */
+$(document).ready(function(){
+	getMessages();
+
+	//setInterval(getMessages, 1000);
+
+	var userName = prompt('Please enter your name.');
+
+	setUserName(userName);
+
+	function setUserName(name) {
+		$('#userName').html('@'+name);
+	}
+
+	function getMessages() {
+		$.get(
+			'https://stark-hamlet-3153.herokuapp.com/chats.json',
+			function(chat) {
+				render(chat);
+			},
+			'json'
+			);
+	}
+
+	function postMessages() {
+		$.post(
+			'https://stark-hamlet-3153.herokuapp.com/chats',
+			{	chat: {
+					username: userName,
+					message: $('#message-box').val(),
+				}
+			},
+			'json'
+		);
+	}
+
+	var render = function(chat) {
+		var messageRow = _.template('<div class="row"><span>@<%= username %>:  </span><span><%= message %></span></span>');
+		for(var i=0; i<chat.length; i++) {
+			$('#message-board').append(messageRow(chat[i]));
+		}
+	};
+
+	$('#send-button').click(function() {
+		postMessages();
+		$('#message-box').val('');
+	});
+
+	// $(window).keyup(function(e) {
+	// 	if(e.which === 13) {
+	// 		postMessages();
+	// 		$('#message-box').val('');
+	// 	}
+	// });
+
+	$('#userName').click(function() {
+		userName = prompt('Please enter your name.');
+		setUserName(userName);
+	});
+	
+});
